36494d7e7568d258823d6836b4b0276ca29ffc29
hoabui-dot <ongchunhostu1998@gmail.com>
2022-04-29 16:50:50 +0700

    update image

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -1,13 +1,17 @@
 import React from "react";
 import styles from "./Banner.module.scss";
 import Image from "next/image";
+import {
+  attributes,
+  react,
+} from "../../content/header__background/header__background.md";
 
 const Banner = () => {
   let { thumbnail } = attributes;
 
   return (
     <div className={styles.header__background}>
-      <Image src={thumbnail} alt={"hehe"} />
+      <Image src={thumbnail} alt={"hehe"} width={300} height={300} />
     </div>
   );
 };
