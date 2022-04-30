363f50dd41fe0b6ee3a381f1c85c48e7bf8b2194
hoabui-dot <ongchunhostu1998@gmail.com>
2022-04-30 21:16:58 +0700

    update config yml file

--- components/Description.js
+++ components/Description.js
@@ -1,7 +1,10 @@
 import React from "react";
 import styles from "../styles/Home.module.scss";
+import { attributes } from "../content/Description/Description.md";
 
 const desc = () => {
+  const { body } = attributes;
+
   const handleClickBtn = () => {
     const btn = document.getElementsByTagName("a");
     btn.style.boxShadow = "rgb(0 0 0 / 13%) 0px 3px 5px inset";
@@ -12,12 +15,13 @@ const desc = () => {
         <div className={`${styles.description__wrap} ${styles.box__shadow}`}>
           <div className={styles.description__content}>
             <p>
-              Hello! I’m Bui Van Hoa. Junior Web Developer with over 1 years of
+              {body}
+              {/* Hello! I’m Bui Van Hoa. Junior Web Developer with over 1 years of
               experience specializing in front end development. Experienced with
               all stages of the development cycle for dynamic web
               projects.Having an in-depth knowledge including advanced HTML5,
               CSS, CSS3, SASS, LESS, JSON, XML, Java, JavaScript, JQuery,
-              Angular JS. Strong background in management and leadership.
+              Angular JS. Strong background in management and leadership. */}
             </p>
           </div>
           <div className={styles.description__btn}>
